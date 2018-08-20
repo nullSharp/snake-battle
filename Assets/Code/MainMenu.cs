d1e0c6d1b14572f42fab01afecc5384e90e55df6
trollingchar <rodind-1@mail.ru>
2018-08-20 15:06:56 +0300

    snakes scene

---
+++ Assets/Code/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class MainMenu : MonoBehaviour {
+
+	[SerializeField] private Button _mySnakesButton;
+
+	// Use this for initialization
+	private void Start () {
+		_mySnakesButton.onClick.AddListener (() => SceneManager.LoadScene ("Snakes"));
+	}
+
+
+	private void OnDestroy () {
+		_mySnakesButton.onClick.RemoveAllListeners ();
+	}
+
+}
