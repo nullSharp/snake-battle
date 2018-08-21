910f432dee0583cb8f90e8b67621af7cc3d3ddd4
trollingchar <rodind-1@mail.ru>
2018-08-21 18:53:45 +0300

    instant toggle ui element

---
+++ Assets/Code/InstantToggle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+namespace Code {
+
+    public class InstantToggle : Toggle {
+
+        public override void OnPointerDown (PointerEventData eventData) {
+            base.OnPointerClick (eventData);
+        }
+
+
+//        public override void OnPointerUp (PointerEventData eventData) {}
+        public override void OnPointerClick (PointerEventData eventData) {}
+
+    }
+
+}
