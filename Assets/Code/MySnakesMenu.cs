9fd28c54dfe41ae6803c87d78e8ae945647abd54
trollingchar <rodind-1@mail.ru>
2018-08-22 21:15:52 +0300

    input box

--- Assets/Code/MySnakesMenu.cs
+++ Assets/Code/MySnakesMenu.cs
@@ -1,12 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 
 namespace Code {
 
     public class MySnakesMenu : MonoBehaviour {
 
+        [SerializeField] private ToggleGroup _toggleGroup;
         [SerializeField] private GameObject _snakeButtonPrefab;
         [SerializeField] private Transform _list;
+
+
+        private void Start () {
+            LoadSnakes ();
+        }
 
 
         private void LoadSnakes () {
@@ -16,10 +23,15 @@ namespace Code {
         }
 
 
-        private void AddSnakeButton (Snake snake) {
-            Instantiate (_snakeButtonPrefab, _list, false).
-            GetComponent <SnakeButton> ().
-            Snake = snake;
+        public void AddSnakeButton () {
+            AddSnakeButton(new Snake (0, "Новая змея", default(Skin), "", new Program ()));
+        }
+
+
+        public void AddSnakeButton (Snake snake) {
+            var sb = Instantiate (_snakeButtonPrefab, _list, false).GetComponent <SnakeButton> ();
+            sb.Snake = snake;
+            sb.group = _toggleGroup;
         }
 
     }
